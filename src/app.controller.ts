d813d9a82a65a6c69e1f61a5fae16ae7ee818c02
Una <una.petrovic@non.agilent.com>
2021-06-08 10:21:22 +0200

    Create the administrator service and add route to controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { Administrator } from 'entities/administrator.entity';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 @Controller()
 export class AppController {
+  constructor(
+    private administratorService: AdministratorService
+  ) {}
   @Get() // http://localhost:3000/
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Get('/api/administrator')
+  getAllAdmins(): Promise<Administrator[]> {
+    return this.administratorService.getAll();
   }
 }
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { timeStamp } from 'console';
 import { Administrator } from 'entities/administrator.entity';
 import { Repository } from 'typeorm';
 
@@ -9,4 +10,14 @@ export class AdministratorService {
         @InjectRepository(Administrator)
         private readonly administrator : Repository<Administrator>
     ) {}
+
+    //nasa funkcija treba da vrati obecanje da ce vratiti niz administratora 
+    getAll(): Promise<Administrator[]> {
+        return this.administrator.find();
+    }
+
+    //nasa funkcija treba da vrati odredjenog administratora po Id-ju
+    getById(id: number): Promise<Administrator>{
+        return this.administrator.findOne(id);
+    }
 }
