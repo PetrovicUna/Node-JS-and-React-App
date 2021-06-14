02c116d9b2e7c54082a56ba143723c210fb579a7
Una <una.petrovic@non.agilent.com>
2021-06-14 13:30:55 +0200

    Make auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,50 @@
+import { HttpException, HttpStatus, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import { AdministratorService } from "src/services/administrator/administrator.service";
+import * as jwt from 'jsonwebtoken';
+import { JwtDataAdministratorDto } from "src/dtos/administrator/jwt.data.administrator.dto";
+import { jwtSecret } from "config/jwt.secret";
+
+
+export class AuthMiddleware implements NestMiddleware {
+    constructor(private readonly administratorService: AdministratorService) {}
+
+    async use(req: Request , res: Response, next: NextFunction) {
+
+        if(!req.headers.authorization){
+            throw new HttpException('Token not found', HttpStatus.UNAUTHORIZED);
+        }
+
+        const token = req.headers.authorization;
+
+        const jwtData: JwtDataAdministratorDto = jwt.verify(token, jwtSecret);
+
+        if(!jwtData){
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if(jwtData.ip !== req.ip.toString())
+        {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        if(jwtData.ua !== req.headers['user-agent'])
+        {
+            throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        const administrator = await this.administratorService.getById(jwtData.administratorId)
+
+        if(!administrator){
+            throw new HttpException('Account token found', HttpStatus.UNAUTHORIZED);
+        }
+
+        const timestampNow = new Date().getTime() / 1000;
+        
+        if(timestampNow >= jwtData.ext){
+            throw new HttpException('The token has expired', HttpStatus.UNAUTHORIZED);
+        }
+     
+        next();    
+    }
+}
